b13036fa6f7056c726f6f2d3345abe7212fbd5b0
Damian Salata <damiansalata10@gmail.com>
2019-03-12 16:16:24 +0100

    Initial commit

---
+++ ArticlesClassifactionCore/SGMParser.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Collections.Specialized;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.Linq;
+using HtmlAgilityPack;
+
+namespace ArticlesClassifactionCore
+{
+    public class SgmParser
+    {
+        public List<DataModel> ReadAllSgmFromDirectory()
+        {
+            DirectoryInfo d = new DirectoryInfo("../../../Data");
+            FileInfo[] files = d.GetFiles("*.sgm");
+            List<string> filePaths = new List<string>();
+            foreach (FileInfo file in files)
+            {
+                filePaths.Add(file.FullName);
+            }
+            List<DataModel> models = new List<DataModel>();
+            foreach (string filePath in filePaths)
+            {
+                models.AddRange(FromSgml(filePath));
+            }
+
+            return models;
+        }
+        public List<DataModel> FromSgml(string path)
+        {
+            List<DataModel> models = new List<DataModel>();
+            HtmlDocument doc = new HtmlDocument();
+            doc.Load(path);
+
+            var pages = doc.DocumentNode.Descendants("REUTERS");
+
+            foreach (var sgmlNode in pages)
+            {
+                DataModel model = new DataModel();
+
+                var categories = sgmlNode.Descendants("D");
+
+                foreach (var category in categories)
+                {
+                    model.Tags[category.ParentNode.Name] = category.ChildNodes.Select(n => n.InnerText).ToList();
+                }
+
+                string date = sgmlNode.Descendants("DATE")?.FirstOrDefault()?.InnerText;
+                if (date != null)
+                {
+                    DateTime.TryParse(date, out DateTime outDate);
+                    model.Date = outDate;
+                }
+                model.Text.Author = sgmlNode.Descendants("AUTHOR").FirstOrDefault()?.InnerText;
+                model.Unknown= sgmlNode.Descendants("UNKNOWN").FirstOrDefault()?.InnerText;
+                model.Text.Title = sgmlNode.Descendants("TITLE").FirstOrDefault()?.InnerText;
+                model.Text.Body = sgmlNode.Descendants("BODY").FirstOrDefault()?.InnerText;
+                model.Text.Dateline = sgmlNode.Descendants("DATELINE").FirstOrDefault()?.InnerText;
+
+                models.Add(model);
+            }
+            return models;
+        }
+    }
+}
---
+++ ConsoleForTests/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArticlesClassifactionCore;
+
+namespace ConsoleForTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SgmParser parser=new SgmParser();
+            var models=parser.ReadAllSgmFromDirectory().Where(n=>n.Tags.ContainsKey("orgs")).ToList();
+        }
+    }
+}
